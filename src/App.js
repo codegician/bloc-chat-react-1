a5d1271d3e85ba617f724780550acfccaf24477a
Jaimie Chung <jchung@Jaimies-MacBook-Air.local>
2018-04-04 21:08:35 -0400

    merging assignment 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+/*<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>*/
+  var config = {
+    apiKey: "AIzaSyA5YqUXWBzkBmy_xOTrG24M-ckUf7h1_M8",
+    authDomain: "bloc-chat-react-7fb1e.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-7fb1e.firebaseio.com",
+    projectId: "bloc-chat-react-7fb1e",
+    storageBucket: "bloc-chat-react-7fb1e.appspot.com",
+    messagingSenderId: "822112736449",
+  };
+  firebase.initializeApp(config);
+
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <header>
+            <nav>
+              <div><Link to='/roomlist'>Room List</Link></div>
+            </nav>
+          </header>
+            <main>
+              <Route path="/roomlist" component={RoomList} />
+            </main>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
